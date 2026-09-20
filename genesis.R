2185a2c7c5edfcdef8827df5337cb6123a015864
Amir Ebneabbasi <amir.ebne.abbasi@gmail.com>
2026-09-20 01:17:52 +0100

    Update genesis.R

--- genesis.R
+++ genesis.R
@@ -30,7 +30,7 @@ scanAnnot <- ScanAnnotationDataFrame(dat)
 
 # SLURM task ID
 task_id <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
-outcome_var <- paste0("State_", task_id)
+outcome_var <- paste0("pheno_", task_id)
 cat("Running for outcome:", outcome_var, "\n")
 
 # NOTE:
@@ -68,62 +68,62 @@ nullmod <- fitNullModel(
 
 cat("Null model fitted for", outcome_var, "\n")
 
-# Load genotype data
-geno <- GdsGenotypeReader(gds_file)
-genoData = GenotypeData(geno)
+# # Load genotype data
+# geno <- GdsGenotypeReader(gds_file)
+# genoData = GenotypeData(geno)
 
-genoiter <- GenotypeBlockIterator(
-  genoData,
-  snpBlock = 5000
-)
+# genoiter <- GenotypeBlockIterator(
+#   genoData,
+#   snpBlock = 5000
+# )
 
-# Single SNP association testing
-assoc <- assocTestSingle(
-  genoiter,
-  null.model = nullmod,
-  BPPARAM = MulticoreParam(workers = 16)
-)
+# # Single SNP association testing
+# assoc <- assocTestSingle(
+#   genoiter,
+#   null.model = nullmod,
+#   BPPARAM = MulticoreParam(workers = 16)
+# )
 
-assoc_rds_file <- file.path(outdir, paste0("assoc_", task_id, ".rds"))
-saveRDS(assoc, file = assoc_rds_file)
-cat("Association results saved to:", assoc_rds_file, "\n")
+# assoc_rds_file <- file.path(outdir, paste0("assoc_", task_id, ".rds"))
+# saveRDS(assoc, file = assoc_rds_file)
+# cat("Association results saved to:", assoc_rds_file, "\n")
 
-# Heritability estimation
-h2 <- varCompCI(nullmod, prop = TRUE)
+# # Heritability estimation
+# h2 <- varCompCI(nullmod, prop = TRUE)
 
-h2_file <- file.path(outdir, paste0("heritability_", task_id, ".rds"))
-saveRDS(h2, file = h2_file)
-cat("Heritability estimates saved to:", h2_file, "\n")
+# h2_file <- file.path(outdir, paste0("heritability_", task_id, ".rds"))
+# saveRDS(h2, file = h2_file)
+# cat("Heritability estimates saved to:", h2_file, "\n")
 
-# Format GWAS results (fastGWA format)
-assoc_tsv_file <- file.path(outdir, paste0("GWAS_", task_id, ".txt"))
+# # Format GWAS results (fastGWA format)
+# assoc_tsv_file <- file.path(outdir, paste0("GWAS_", task_id, ".txt"))
 
-alleles <- tibble(
-  SNP = getSnpID(genoData),
-  A1  = getAlleleA(genoData),
-  A2  = getAlleleB(genoData)
-)
+# alleles <- tibble(
+#   SNP = getSnpID(genoData),
+#   A1  = getAlleleA(genoData),
+#   A2  = getAlleleB(genoData)
+# )
 
-assoc %>%
-  as_tibble() %>%
-  select(-MAC) %>%
-  rename(
-    SNP  = variant.id,
-    CHR  = chr,
-    POS  = pos,
-    N    = n.obs,
-    AF1  = freq,
-    BETA = Est,
-    SE   = Est.SE,
-    P    = Score.pval
-  ) %>%
-  inner_join(alleles, by = "SNP") %>%
-  mutate(N = N / 2) %>%
-  select(CHR, SNP, POS, A1, A2, N, AF1, BETA, SE, P) %>%
-  write_tsv(assoc_tsv_file)
+# assoc %>%
+#   as_tibble() %>%
+#   select(-MAC) %>%
+#   rename(
+#     SNP  = variant.id,
+#     CHR  = chr,
+#     POS  = pos,
+#     N    = n.obs,
+#     AF1  = freq,
+#     BETA = Est,
+#     SE   = Est.SE,
+#     P    = Score.pval
+#   ) %>%
+#   inner_join(alleles, by = "SNP") %>%
+#   mutate(N = N / 2) %>%
+#   select(CHR, SNP, POS, A1, A2, N, AF1, BETA, SE, P) %>%
+#   write_tsv(assoc_tsv_file)
 
-cat("Formatted GWAS output saved to:", assoc_tsv_file, "\n")
+# cat("Formatted GWAS output saved to:", assoc_tsv_file, "\n")
 
-# Close GDS connection
-close(genoData)
-cat("GDS connection closed\n")
+# # Close GDS connection
+# close(genoData)
+# cat("GDS connection closed\n")
